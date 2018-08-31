1d9f914058dd40194594fa292f6435274f369e35
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-09-01 01:50:31 +0300

    Dired config added

---
+++ etc/editor/use-dired.el
@@ -0,0 +1,20 @@
+;;; use-dired.el ---  dired mode settings. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+(use-package dired :ensure nil
+  :config
+  (progn
+    (setq dired-dwim-target         t
+          dired-recursive-copies    'always
+          dired-recursive-deletes   'top   )
+
+    (put 'dired-find-alternate-file 'disabled nil)))
+
+(use-package dired+ :ensure quelpa
+  :config (diredp-toggle-find-file-reuse-dir 1)
+  :quelpa (dired+ :fetcher wiki))
+
+(use-package dired-x :ensure nil)
+
+(provide 'use-dired)
+;;; set-dired.el ends here
--- etc/set-editor.el
+++ etc/set-editor.el
@@ -8,14 +8,6 @@
 (setq bookmark-default-file (expand-file-name "bookmarks.el" vs-emacs-cache-dir)
       bookmark-save-flag    1)
 
-(require 'dired)
-(setq dired-dwim-target       t
-      dired-recursive-copies  'always
-      dired-recursive-deletes 'top)
-;; dired - reuse current buffer by pressing 'a'
-(put 'dired-find-alternate-file 'disabled nil)
-(require 'dired-x)
-
 ;; ediff - don't start another frame
 (require 'ediff)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
@@ -28,10 +20,6 @@
 (with-region-or-buffer indent-region)
 (with-region-or-buffer untabify)
 
-;; make a shell script executable automatically on save
-(add-hook 'after-save-hook
-          'executable-make-buffer-file-executable-if-script-p)
-
 ;; saner regex syntax
 (require 're-builder)
 (setq reb-re-syntax 'string)
@@ -40,6 +28,10 @@
 (setq eshell-directory-name (expand-file-name "eshell" vs-emacs-cache-dir))
 
 (vs//require-dir (expand-file-name "editor" vs-emacs-config-dir))
+
+;; make a shell script executable automatically on save
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
 
 (provide 'set-editor)
 ;;; set-editor.el ends here
