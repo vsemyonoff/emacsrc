88f215207d5ff6b3675a6d254247963d11c4cfb9
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-08-31 18:23:27 +0300

    Apache Velocity mode for '.tpl' files enabled

---
+++ etc/editor/use-vtl.el
@@ -0,0 +1,10 @@
+;;; use-vtl.el ---  Apache Velocity templates mode settings. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+(use-package vtl
+  :load-path (lambda ()
+               (expand-file-name "site-lisp" vs-emacs-data-dir))
+  :mode ("\\.tpl\\'" . turn-on-vtl-mode))
+
+(provide 'use-vtl)
+;;; set-vtl.el ends here
