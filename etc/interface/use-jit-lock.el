69691b65a17d8cfa8f7ba0bd8ae08c418fe9f982
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-09-03 20:58:36 +0300

    Font lock tweaks

--- etc/interface/use-jit-lock.el
+++ etc/interface/use-jit-lock.el
@@ -2,11 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 (use-package jit-lock :ensure nil
-  :config (setq jit-lock-defer-time      nil
-                jit-lock-stealth-nice    0.1
-                jit-lock-stealth-time    0.2
-                jit-lock-stealth-verbose nil)
-  :hook (change-major-mode . (lambda () (jit-lock-mode t))))
+  :config
+  (progn
+    (setq-default font-lock-multiline t)
+    (setq font-lock-support-mode      'jit-lock-mode
+          jit-lock-defer-time         nil
+          jit-lock-stealth-nice       0.1
+          jit-lock-stealth-time       0.2
+          jit-lock-stealth-verbose    nil)))
 
 (provide 'use-jit-lock)
 ;;; use-jit-lock.el ends here
