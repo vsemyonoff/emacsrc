418b13e60f512fc86b2e2ee112967d3ccdc4af27
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-09-04 12:44:33 +0300

    Company mode tweaks

--- etc/editor/use-company.el
+++ etc/editor/use-company.el
@@ -2,6 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 (use-package company :delight
+  :config (setq company-dabbrev-code-ignore-case    t
+                company-dabbrev-ignore-case         t
+                company-etags-ignore-case           t
+                company-idle-delay                  0.1
+                company-minimum-prefix-length       2
+                company-require-match               nil
+                company-tooltip-flip-when-above     t
+                company-tooltip-minimum-width       50
+                company-tooltip-align-annotations   t
+                company-tooltip-margin              2  )
   :hook (vs-emacs-config . global-company-mode))
 
 (provide 'use-company)
