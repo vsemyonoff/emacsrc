c3e604fc0192c57e10909af988707b370668b468
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-09-06 00:09:29 +0300

    Treemacs cleanup

--- etc/interface/use-treemacs.el
+++ etc/interface/use-treemacs.el
@@ -9,6 +9,9 @@
   (progn
     (setq treemacs-collapse-dirs            3
           treemacs-follow-after-init        t
+          treemacs-indentation-string       "  "
+          treemacs-indentation              1
+          treemacs-is-never-other-window    t
           treemacs-persist-file             (expand-file-name "treemacs.el" vs-emacs-cache-dir)
           treemacs-show-hidden-files        nil
           treemacs-silent-refresh           t
@@ -17,9 +20,9 @@
 
     (treemacs-filewatch-mode                t)
     (treemacs-follow-mode                   t)
-    (treemacs-git-mode                      'extended)
+    (treemacs-git-mode                      'simple)
 
-    (defun vs//treemacs-add-project (&optional path)
+    (defun vs|treemacs//add-project (&optional path)
       "Add project from `PATH' to `treemacs'."
       (let* ((path (or path vs-user-home-dir))
              (name (file-name-nondirectory
@@ -30,107 +33,74 @@
           (treemacs--find-workspace))
         (treemacs-do-add-project-to-workspace path name)))
 
-    (defun vs|treemacs-no-fringes ()
-      (when (display-graphic-p)
-        (set-window-fringes nil 0 0)))
+    (defun vs|treemacs|mode-setup ()
+      "Set `treemacs' mode local variables."
+      (setq indicate-buffer-boundaries nil
+            indicate-empty-lines       nil
+            line-spacing               1
+            tab-width                  1  ))
 
-    (defun vs|treemacs-setup (&rest _)
-      (setq line-spacing 1
-            tab-width    1))
-
-    (defun vs//treemacs-variable-pitch-labels (&rest _)
-      (dolist (face '(treemacs-root-face
-                      treemacs-git-unmodified-face
-                      treemacs-git-modified-face
-                      treemacs-git-renamed-face
-                      treemacs-git-ignored-face
-                      treemacs-git-untracked-face
-                      treemacs-git-added-face
-                      treemacs-git-conflict-face
-                      treemacs-directory-face
-                      treemacs-directory-collapsed-face
-                      treemacs-file-face))
-        (let ((faces (face-attribute face :inherit nil)))
-          (set-face-attribute
-           face nil :inherit
-           `(variable-pitch ,@(delq 'unspecified (if (listp faces) faces (list faces))))))))
-
-    (defun vs|treemacs-config ()
+    (defun vs|treemacs|icons-setup ()
       (unless (require 'all-the-icons nil t)
         (error "all-the-icons isn't installed"))
 
       ;; Silence plistp error with all-the-icons
-      (advice-add #'treemacs--pulse-png-advice :override #'ignore)
+      (advice-add 'treemacs--pulse-png-advice :override 'ignore)
 
-      (add-hook 'treemacs-mode-hook #'vs|treemacs-setup)
-
-      ;; no fringes in treemacs window
-      (add-hook 'treemacs-mode-hook #'vs|treemacs-no-fringes)
-      (advice-add #'treemacs-select-window :after #'vs|treemacs-no-fringes)
-
-      ;; variable-pitch labels for files/folders
-      (vs//treemacs-variable-pitch-labels)
-      (advice-add #'load-theme :after #'vs//treemacs-variable-pitch-labels)
-
-      ;; minimalistic atom-inspired icon theme
       (let ((all-the-icons-default-adjust 0))
-        (setq treemacs-icon-root-png
-              (concat " " (all-the-icons-octicon "repo" :v-adjust -0.1 :height 1.6
-                                                 :face 'font-lock-string-face)
-                      " ")
-
-              treemacs-icon-tag-open-png
-              (all-the-icons-octicon "chevron-down" :v-adjust 0.1)
-              treemacs-icon-tag-closed-png
-              (all-the-icons-octicon "chevron-right" :v-adjust 0.1)
-
-              treemacs-indentation-string "  "
-              treemacs-indentation 1
-
-              treemacs-icon-open-png
-              (concat (all-the-icons-octicon "file-directory" :v-adjust 0 :height 1.15)
-                      " ")
-              treemacs-icon-closed-png
-              (concat (all-the-icons-octicon "file-directory" :v-adjust 0 :height 1.15 :face 'font-lock-doc-face)
-                      " ")
-
-              treemacs-icon-tag-node-open-png
-              (concat (all-the-icons-octicon "chevron-down"  :height 0.75 :face 'font-lock-keyword-face)
-                      "\t")
-              treemacs-icon-tag-node-closed-png
-              (concat (all-the-icons-octicon "chevron-right" :height 0.9  :face 'font-lock-keyword-face)
-                      "\t")
-              treemacs-icon-tag-leaf-png "- ")
-
-        ;; File type icons
-        (setq treemacs-icons-hash (make-hash-table :size 200 :test #'equal)
-              treemacs-icon-fallback (concat (all-the-icons-octicon "file-code" :height 1.2 :v-adjust 0) " ")
+        ;; Folders
+        (setq treemacs-icon-closed-png          (concat (all-the-icons-octicon "file-directory"
+                                                                               :v-adjust 0 :height 1.15
+                                                                               :face 'font-lock-doc-face)
+                                                        " ")
+              treemacs-icon-open-png            (concat (all-the-icons-octicon "file-directory" :v-adjust 0 :height 1.15)
+                                                        " ")
+              treemacs-icon-root-png            (concat " " (all-the-icons-octicon "repo"
+                                                                                   :v-adjust -0.1 :height 1.6
+                                                                                   :face 'font-lock-string-face)
+                                                        " ")
+              treemacs-icon-tag-closed-png      (concat (all-the-icons-octicon "chevron-right"
+                                                                               :v-adjust 0.1)
+                                                        " ")
+              treemacs-icon-tag-node-closed-png (concat (all-the-icons-octicon "chevron-right"
+                                                                               :height 0.9
+                                                                               :face 'font-lock-keyword-face)
+                                                        " ")
+              treemacs-icon-tag-node-open-png   (concat (all-the-icons-octicon "chevron-down"
+                                                                               :height 0.75
+                                                                               :face 'font-lock-keyword-face)
+                                                        " ")
+              treemacs-icon-tag-open-png        (concat (all-the-icons-octicon "chevron-down" :v-adjust 0.1)
+                                                        " ")
+              treemacs-icon-tag-leaf-png        "- ")
+        ;; Files
+        (setq treemacs-icons-hash    (make-hash-table :size 200 :test #'equal)
+              treemacs-icon-fallback (concat (all-the-icons-octicon "file-code" :height 1.2)
+                                             " ")
               treemacs-icon-text     treemacs-icon-fallback)
-
+        ;; File types
         (treemacs-define-custom-icon (all-the-icons-octicon "file-media" :height 1.2)
-                                     "png" "jpg" "jpeg" "gif" "ico" "tif" "tiff" "svg" "bmp"
-                                     "psd" "ai" "eps" "indd" "mov" "avi" "mp4" "webm" "mkv"
-                                     "wav" "mp3" "ogg" "midi")
-
+                                     "png" "jpg" "jpeg" "gif"  "ico" "tif" "tiff" "svg"  "bmp"
+                                     "psd" "ai"  "eps"  "indd" "mov" "avi" "mp4"  "webm" "mkv"
+                                     "wav" "mp3" "ogg"  "midi")
         (treemacs-define-custom-icon (all-the-icons-octicon "file-text" :height 1.2)
                                      "md" "markdown" "rst" "log" "org" "txt"
                                      "CONTRIBUTE" "LICENSE" "README" "CHANGELOG")
-
         (treemacs-define-custom-icon (all-the-icons-octicon "file-code" :height 1.2)
-                                     "yaml" "yml" "json" "xml" "toml" "cson" "ini"
-                                     "tpl" "erb" "mustache" "twig" "ejs" "mk" "haml" "pug" "jade"))))
+                                     "json" "xml" "ini" "tpl" "mk"))))
 
-  :hook (vs-emacs-config-gui . vs|treemacs-config))
+  :hook ((treemacs-mode       . vs|treemacs|mode-setup )
+         (vs-emacs-config-gui . vs|treemacs|icons-setup)))
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
-  :bind (:map global-map ([C-tab] . vs/treemacs-open))
+  :bind (:map global-map ([C-tab] . vs|treemacs|open))
   :config
   (progn
-    (defun vs/treemacs-open ()
-      "Setup project for current file and open `treemacs' buffer."
+    (defun vs|treemacs|open ()
+      "Add project for current file and open `treemacs' buffer."
       (interactive)
-      (vs//treemacs-add-project (condition-case _
+      (vs|treemacs//add-project (condition-case _
                                     (expand-file-name (projectile-project-root))
                                   (error (expand-file-name default-directory))))
       (treemacs-select-window))))
