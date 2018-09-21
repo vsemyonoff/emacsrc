6ed567da34fe34a513dca16fb1225e11ff0c1280
Vladislav Semyonoff <vsemyonoff@gmail.com>
2018-09-22 02:42:49 +0300

    Parens highlight settings updated

--- etc/interface/use-paren.el
+++ etc/interface/use-paren.el
@@ -3,10 +3,11 @@
 ;;; Code:
 (use-package paren
   :config
-  (setq blink-matching-paren              'jump
+  (setq blink-matching-paren               t
         show-paren-delay                   0.1
         show-paren-highlight-openparen     t
-        show-paren-when-point-inside-paren t   )
+        show-paren-when-point-in-periphery t
+        show-paren-when-point-inside-paren t)
 
   :hook (vs-emacs-config . show-paren-mode))
 
