223e5a23996487d0342da7d8f81924ddc18818cc
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-04-16 23:56:12 +0300

    Dired all-the-icons enabled

--- etc/external/interface/use-all-the-icons.el
+++ etc/external/interface/use-all-the-icons.el
@@ -3,19 +3,20 @@
 ;;; Code:
 (require 'straight)
 
-(straight-use-package 'all-the-icons)
-;; Triggers
-(autoload 'all-the-icons-octicon "all-the-icons")
-(autoload 'all-the-icons-faicon "all-the-icons")
-(autoload 'all-the-icons-fileicon "all-the-icons")
-(autoload 'all-the-icons-wicon "all-the-icons")
-(autoload 'all-the-icons-material "all-the-icons")
-(autoload 'all-the-icons-alltheicon "all-the-icons")
-(autoload 'all-the-icons-install-fonts "all-the-icons")
+(when (straight-use-package 'all-the-icons)
+  ;; Triggers
+  (autoload 'all-the-icons-alltheicon "all-the-icons")
+  (autoload 'all-the-icons-faicon "all-the-icons")
+  (autoload 'all-the-icons-fileicon "all-the-icons")
+  (autoload 'all-the-icons-install-fonts "all-the-icons")
+  (autoload 'all-the-icons-material "all-the-icons")
+  (autoload 'all-the-icons-octicon "all-the-icons")
+  (autoload 'all-the-icons-wicon "all-the-icons")
 
-(straight-use-package 'all-the-icons-dired)
-;; Triggers
-(add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
+  (with-eval-after-load 'dired
+    (when (straight-use-package 'all-the-icons-dired)
+      ;; Triggers
+      (add-hook 'dired-mode-hook #'all-the-icons-dired-mode))))
 
 (provide 'use-all-the-icons)
 ;;; use-all-the-icons.el ends here
