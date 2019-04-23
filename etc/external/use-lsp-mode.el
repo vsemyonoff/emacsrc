d29a7afc5eddd81ac4bdab2cbf81e84de80ea527
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-04-23 12:10:51 +0300

    LSP 'ccls' backend enabled

--- etc/external/use-lsp-mode.el
+++ etc/external/use-lsp-mode.el
@@ -8,36 +8,30 @@
     (warn "===> Can't install 'lsp-mode'")
 
   ;; Triggers
-  (add-hook 'c++-mode-hook    #'lsp)
-  (add-hook 'c-mode-hook      #'lsp)
   (add-hook 'css-mode-hook    #'lsp)
   (add-hook 'html-mode-hook   #'lsp)
   (add-hook 'python-mode-hook #'lsp)
 
   ;; Config
   (with-eval-after-load 'lsp
-    (setq lsp-auto-configure     nil
+    (setq lsp-auto-guess-root    t
           lsp-eldoc-enable-hover nil
           lsp-eldoc-render-all   nil
           lsp-inhibit-message    nil
-          lsp-restart            'auto-restart)
+          lsp-prefer-flymake     nil
+          lsp-restart            'auto-restart
+          lsp-session-file       (expand-file-name "lsp.session"
+                                                   vs-emacs-cache-dir))
     )
 
   (if (not (straight-use-package 'lsp-ui))
       (warn "===> Can't install 'lsp-ui'")
 
-    ;; Triggers
+    ;; Triggers/config
     (with-eval-after-load 'lsp
-      (add-hook 'lsp-mode-hook #'lsp-ui-mode)
-      )
+      (require 'lsp-ui)
 
-    ;; Config
-    (with-eval-after-load 'lsp-ui
-      (defun vs|lsp-ui/setup ()
-        (eldoc-mode            -1)
-        (lsp-ui-flycheck-enable 1)
-        )
-      (add-hook 'lsp-ui-mode-hook #'vs|lsp-ui/setup)
+      (add-hook 'lsp-ui-mode-hook (lambda () (eldoc-mode -1)))
 
       ;; Keybindings
       (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
@@ -47,10 +41,9 @@
       (setq lsp-ui-doc-use-childframe         t
             lsp-ui-flycheck-enable            t
             lsp-ui-sideline-ignore-duplicate  t
-            lsp-ui-sideline-show-code-actions t
+            lsp-ui-sideline-show-code-actions nil
             lsp-ui-sideline-show-flycheck     t
             lsp-ui-sideline-show-hover        nil
-            lsp-ui-sideline-show-symbol       t
             lsp-ui-sideline-show-symbol       nil
             lsp-ui-sideline-update-mode       'point)
       )
@@ -60,56 +53,39 @@
     (if (not (straight-use-package 'company-lsp))
         (warn "===> Can't install 'company-lsp'")
 
-      ;; Triggers
+      ;; Triggers/config
       (with-eval-after-load 'lsp
-        (defun vs|company-lsp/enable ()
-          (make-local-variable 'company-backends)
-          (push 'company-lsp company-backends)
-          )
-        (add-hook 'lsp-mode-hook #'vs|company-lsp/enable)
-        )
+        (require 'company-lsp)
 
-      ;; Config
-      (with-eval-after-load 'company-lsp
-        ;; ;; From cquery wiki page
-        ;; (setq company-lsp-cache-candidates nil
-        ;;       company-transformers         nil)
         (setq company-lsp-async            t
               company-lsp-enable-snippet   t
-              company-lsp-cache-candidates t)
+              company-lsp-cache-candidates nil)
         )
       )
     )
 
-  ;; (if (not (straight-use-package 'cquery))
-  ;;     (warn "===> Can't install 'cquery'")
+  (if (not (straight-use-package 'ccls))
+      (warn "===> Can't install 'ccls'")
 
-  ;;   ;; Triggers
-  ;;   (defun vs|cquery/enable ()
-  ;;     (require 'cquery)
-  ;;     (lsp)
-  ;;     )
-  ;;   (add-hook 'c++-mode-hook #'vs|cquery/enable)
-  ;;   (add-hook 'c-mode-hook   #'vs|cquery/enable)
+    ;; Triggers
+    (defun vs|ccls/enable ()
+      (require 'ccls)
+      (lsp)
+      )
+    (add-hook 'c++-mode-hook    #'vs|ccls/enable)
+    (add-hook 'c-mode-hook      #'vs|ccls/enable)
+    (add-hook 'objc-mode-hook   #'vs|ccls/enable)
 
-  ;;   ;; Config
-  ;;   (with-eval-after-load 'cquery
-  ;;     (with-eval-after-load 'projectile
-  ;;       (add-to-list 'projectile-project-root-files-top-down-recurring ".cquery")
-  ;;       )
+    ;; Config
+    (with-eval-after-load 'ccls
+      (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
 
-  ;;     (with-eval-after-load 'company-lsp
-  ;;       (defun vs|cquery/company-lsp-setup ()
-  ;;         (set (make-local-variable 'company-lsp-cache-candidates) nil)
-  ;;         )
-  ;;       (add-hook 'company-mode-hook #'vs|cquery/company-lsp-setup)
-  ;;       )
-
-  ;;     (setq cquery-extra-init-params '(:index (:comments 2)
-  ;;                                      :cacheFormat "msgpack"
-  ;;                                      :completion (:detailedLabel t)))
-  ;;     )
-  ;;   )
+      (with-eval-after-load 'projectile
+        (add-to-list 'projectile-project-root-files-top-down-recurring ".ccls")
+        (add-to-list 'projectile-project-root-files-bottom-up ".ccls")
+        )
+      )
+    )
 
   (if (not (straight-use-package 'lsp-java))
       (warn "===> Can't install 'lsp-java'")
@@ -123,13 +99,6 @@
 
     ;; Config
     (with-eval-after-load 'lsp-java
-      (with-eval-after-load 'lsp-ui
-        (defun vs|lsp-java/lsp-ui-setup ()
-          (set (make-local-variable 'lsp-ui-sideline-show-code-actions) nil)
-          )
-        (add-hook 'lsp-ui-mode-hook #'vs|lsp-java/lsp-ui-setup)
-        )
-
       (setq lsp-java-server-install-dir  (expand-file-name "eclipse/" vs-xdg-data-dir  )
             lsp-java-workspace-cache-dir (expand-file-name "eclipse/" vs-xdg-cache-dir )
             lsp-java-workspace-dir       (expand-file-name "eclipse/" vs-xdg-config-dir))
