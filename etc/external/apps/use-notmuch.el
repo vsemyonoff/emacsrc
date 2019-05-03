bce159551f663dcd21f1bc48d677f014e2f2688b
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-05-03 19:12:34 +0300

    Notmuch config moved to 'easymail-init.el'

--- etc/external/apps/use-notmuch.el
+++ etc/external/apps/use-notmuch.el
@@ -4,71 +4,10 @@
 (if (not (straight-use-package 'notmuch))
     (warn "===> Can't install 'notmuch'")
 
+  (setq notmuch-init-file (expand-file-name "easymail-init" user-emacs-directory))
+
   (global-set-key (kbd "C-c m") #'notmuch)
 
-  (with-eval-after-load 'notmuch
-    (setq mm-text-html-renderer 'lynx
-          notmuch-hello-hide-tags '("attachment" "replied" "unread")
-          notmuch-hello-sections '(notmuch-hello-insert-header
-                                   notmuch-hello-insert-saved-searches
-                                   notmuch-hello-insert-search
-                                   notmuch-hello-insert-alltags
-                                   notmuch-hello-insert-footer)
-          notmuch-search-oldest-first nil
-          notmuch-show-all-tags-list t
-          notmuch-show-logo nil
-          )
-
-    (setq notmuch-saved-searches
-          '(
-            (:name "Unread"  :query "tag:unread"                   :key "u")
-            (:name "Archive" :query "folder:\"/[^\/]+\/Archive/\"" :key "a")
-            (:name "Inbox"   :query "folder:\"/[^\/]+\/Inbox/\""   :key "i")
-            (:name "Drafts"  :query "folder:\"/[^\/]*\/Drafts/\""  :key "d")
-            (:name "Sent"    :query "folder:\"/[^\/]+\/Sent/\""    :key "s")
-            (:name "Spam"    :query "folder:\"/[^\/]+\/Spam/\""    :key "S")
-            (:name "Trash"   :query "folder:\"/[^\/]+\/Trash/\""   :key "t")
-            )
-          )
-
-    (defun vs|notmuch|tree-toggle-deleted ()
-      "Toggle 'deleted' tag for message"
-      (interactive)
-      (notmuch-tree-tag
-       (if (member "deleted" (notmuch-tree-get-tags))
-           (list "-deleted")
-         (list "+deleted")
-         )
-       )
-      )
-    (define-key notmuch-tree-mode-map   "d" #'vs|notmuch|tree-toggle-deleted)
-    (define-key notmuch-show-mode-map   "d" #'vs|notmuch|tree-toggle-deleted)
-
-    (defun vs|notmuch|search-toggle-deleted ()
-      "Toggle 'deleted' tag for message."
-      (interactive)
-      (notmuch-search-tag
-       (if (member "deleted" (notmuch-search-get-tags))
-           (list "-deleted")
-         (list "+deleted")
-         )
-       )
-      )
-    (define-key notmuch-search-mode-map "d" #'vs|notmuch|search-toggle-deleted)
-
-    (defun vs|notmuch|search-toggle-unread ()
-      "Toggle 'unread' tag for message."
-      (interactive)
-      (notmuch-search-tag
-       (if (member "unread" (notmuch-search-get-tags))
-           (list "-unread")
-         (list "+unread")
-         )
-       )
-      )
-    (define-key notmuch-search-mode-map (kbd "<tab>") #'vs|notmuch|search-toggle-unread)
-
-    )
   )
 
 (provide 'use-notmuch)
