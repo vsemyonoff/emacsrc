86ab2a64e40661b601f62dc74c17821309c25574
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-05-07 23:24:27 +0300

    Notmuch change context when compositing message

--- etc/external/apps/use-notmuch.el
+++ etc/external/apps/use-notmuch.el
@@ -5,9 +5,18 @@
     (warn "===> Can't install 'notmuch'")
 
   (setq notmuch-init-file (expand-file-name "easymail-init" user-emacs-directory))
-
   (global-set-key (kbd "C-c m") #'notmuch)
 
+  ;; Multiple identities
+  (if (not (straight-use-package 'gnus-alias))
+      (warn "===> Can't install 'gnus-alias'")
+
+    (add-hook 'message-setup-hook #'gnus-alias-determine-identity)
+
+    (with-eval-after-load 'message
+      (define-key message-mode-map (kbd "<f12>") #'gnus-alias-select-identity)
+      )
+    )
   )
 
 (provide 'use-notmuch)
