4b3257562abadf2abb37417c5efc684263b95a4b
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-05-12 08:11:25 +0300

    Jump to paren function

--- etc/builtins/set-paren.el
+++ etc/builtins/set-paren.el
@@ -10,6 +10,16 @@
         show-paren-when-point-in-periphery t
         show-paren-when-point-inside-paren t
         )
+
+  (defun vs|paren|match-paren (arg)
+    "Go to the matching paren if on a paren; otherwise insert %."
+    (interactive "p")
+    (cond ((looking-at "\\s(") (forward-list 1) (backward-char 1))
+          ((looking-at "\\s)") (forward-char 1) (backward-list 1))
+          (t (self-insert-command (or arg 1)))
+          )
+    )
+  (global-set-key "%" #'vs|paren|match-paren)
   )
 
 (provide 'set-paren)
